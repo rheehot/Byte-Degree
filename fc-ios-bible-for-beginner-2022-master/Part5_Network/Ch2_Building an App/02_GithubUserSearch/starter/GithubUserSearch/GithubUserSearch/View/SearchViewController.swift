70d9e12e57990614999a5cf7a2cbfdc1be59b434
devbbang <ios_developer.th@icloud.com>
2022-08-01 23:59:33 +0900

    project "load github profile"

--- fc-ios-bible-for-beginner-2022-master/Part5_Network/Ch2_Building an App/02_GithubUserSearch/starter/GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
+++ fc-ios-bible-for-beginner-2022-master/Part5_Network/Ch2_Building an App/02_GithubUserSearch/starter/GithubUserSearch/GithubUserSearch/View/SearchViewController.swift
@@ -10,9 +10,103 @@ import Combine
 
 class SearchViewController: UIViewController {
     
+    @Published private(set) var users: [SearchResult] = []
+    var subcriptions = Set<AnyCancellable>()
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    typealias Item = SearchResult
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
+    enum Section {
+        case main
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        embedSearchControl()
+        configureCollectionView()
+        bind()
+    }
+    
+    private func embedSearchControl() {
+        self.navigationItem.title = "Search"
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "devbbang"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        self.navigationItem.searchController = searchController
+    }
+    
+    private func configureCollectionView() {
+        datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else { return nil }
+            
+            cell.user.text = item.login
+            return cell
+        })
+        
+        collectionView.collectionViewLayout = layout()
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    private func bind() {
+        $users
+            .receive(on: RunLoop.main)
+            .sink { users in
+                var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+                snapshot.appendSections([.main])
+                snapshot.appendItems(users, toSection: .main)
+                self.datasource.apply(snapshot)
+            }.store(in: &subcriptions)
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text
+        print("search: \(keyword)")
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("button clicked: \(searchBar.text)")
+        
+        guard let keyword = searchBar.text, !keyword.isEmpty else { return }
+        let base = "https://api.github.com/"
+        let path = "search/users"
+        let params: [String: String] = ["q": keyword]
+        let header: [String: String] = ["Content-Type": "application/json"]
+        
+        var urlComponents = URLComponents(string: base + path)!
+        let queryItems = params.map { (key: String, value: String) in
+            return URLQueryItem(name: key, value: value)
+        }
+        urlComponents.queryItems = queryItems
+        
+        var request = URLRequest(url: urlComponents.url!)
+        header.forEach { (key: String, value: String) in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        URLSession.shared.dataTaskPublisher(for: request)
+            .map { $0.data }
+            .decode(type: SearchUserResponse.self, decoder: JSONDecoder())
+            .map { $0.items }
+            .replaceError(with: [])
+            .receive(on: RunLoop.main)
+            .assign(to: \.users, on: self)
+            .store(in: &subcriptions)
     }
 }
