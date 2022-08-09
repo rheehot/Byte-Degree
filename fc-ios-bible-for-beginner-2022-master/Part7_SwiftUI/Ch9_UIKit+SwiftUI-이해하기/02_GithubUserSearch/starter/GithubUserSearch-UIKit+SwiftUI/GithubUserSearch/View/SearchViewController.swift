39024b178f4c5c709fc78cf90bf0c63ed12c586e
devbbang <ios_developer.th@icloud.com>
2022-08-09 20:46:23 +0900

    project "github user search" with UIKit & SwiftUI

--- fc-ios-bible-for-beginner-2022-master/Part7_SwiftUI/Ch9_UIKit+SwiftUI-이해하기/02_GithubUserSearch/starter/GithubUserSearch-UIKit+SwiftUI/GithubUserSearch/View/SearchViewController.swift
+++ fc-ios-bible-for-beginner-2022-master/Part7_SwiftUI/Ch9_UIKit+SwiftUI-이해하기/02_GithubUserSearch/starter/GithubUserSearch-UIKit+SwiftUI/GithubUserSearch/View/SearchViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Combine
+import SwiftUI
 
 class SearchViewController: UIViewController {
     
@@ -48,8 +49,8 @@ class SearchViewController: UIViewController {
             return cell
         })
         
-        
         collectionView.collectionViewLayout = layout()
+        collectionView.delegate = self
     }
     
     private func bind() {
@@ -74,7 +75,6 @@ class SearchViewController: UIViewController {
     }
 }
 
-
 extension SearchViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         let keyword = searchController.searchBar.text
@@ -86,5 +86,17 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let keyword = searchBar.text, !keyword.isEmpty else { return }
         viewModel.search(keyword: keyword)
+    }
+}
+
+extension SearchViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("--> loginID: \(viewModel.users[indexPath.item].login)")
+        
+        let loginID = viewModel.users[indexPath.item].login
+        let viewModel = UserProfileViewModel(network: NetworkService(configuration: .default), loginID: loginID)
+        let userProfileView = UserProfileView(viewModel: viewModel)
+        let hostingVC = UIHostingController(rootView: userProfileView)
+        navigationController?.pushViewController(hostingVC, animated: true)
     }
 }
