bef398142a325185db3216d1b46c7b9b05df3125
devbbang <ios_developer.th@icloud.com>
2022-08-23 21:00:55 +0900

    project "CarrotHomeTab" complete

---
+++ fc-ios-bible-for-beginner-2022-master/Part8_Projects/02-Carrot_Home/starter/CarrotHomeTab/CarrotHomeTab/Detail/DetailViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewModel.swift
+//  CarrotHomeTab
+//
+//  Created by TAEJANIM on 2022/08/23.
+//
+
+import Foundation
+import Combine
+
+final class DetailViewModel {
+    
+    let network: NetworkService
+    let itemInfo: ItemInfo
+    
+    @Published var itemInfoDetails: ItemInfoDetails? = nil
+    
+    init(network: NetworkService, itemInfo: ItemInfo) {
+        self.network = network
+        self.itemInfo = itemInfo
+    }
+    
+    func fetch() {
+        // simulate network like behavior
+        DispatchQueue.global().asyncAfter(deadline: .now() + 0.3) { [unowned self] in
+            self.itemInfoDetails = ItemInfoDetails(user: User.mock, item: self.itemInfo, details: ItemExtraInfo.mock)
+        }
+//        let resource: Resource<ItemInfoDetails> = Resource(base: <#T##String#>, path: <#T##String#>, params: [:], header: [:])
+//
+//        network.load(resource)
+//            .receive(on: RunLoop.main)
+//            .sink(
+    }
+}
--- fc-ios-bible-for-beginner-2022-master/Part8_Projects/02-Carrot_Home/starter/CarrotHomeTab/CarrotHomeTab/Home/ItemInfoCell.swift
+++ fc-ios-bible-for-beginner-2022-master/Part8_Projects/02-Carrot_Home/starter/CarrotHomeTab/CarrotHomeTab/Home/ItemInfoCell.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import Kingfisher
+
+// - [✅] 셀에서, 콤마 표시하게끔 넘버포매팅
+// - [✅] 셀에서, 이미지 세팅하기 (+ cornerRadius 설정)
 
 class ItemInfoCell: UICollectionViewCell {
     
@@ -16,12 +20,32 @@ class ItemInfoCell: UICollectionViewCell {
     @IBOutlet weak var numberOfChatLabel: UILabel!
     @IBOutlet weak var numberOfLikeLabel: UILabel!
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        thumbnail.layer.cornerRadius = 10
+        thumbnail.layer.masksToBounds = true
+        thumbnail.tintColor = .systemGray
+    }
+    
     func configure(item: ItemInfo) {
         titleLabel.text = item.title
         descriptionLabel.text = item.location
-        priceLabel.text = "\(item.price)원"
+        priceLabel.text = "\(formatNumber(item.price))원"
         
         numberOfChatLabel.text = "\(item.numOfChats)"
         numberOfLikeLabel.text = "\(item.numOfLikes)"
+        
+        thumbnail.kf.setImage(
+            with: URL(string: item.thumbnailURL)!,
+            placeholder: UIImage(systemName: "hands.sparkles.fill")
+        )
+    }
+    
+    private func formatNumber(_ price: Int) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        let result = formatter.string(from: NSNumber(integerLiteral: price)) ?? ""
+        return result
     }
 }
